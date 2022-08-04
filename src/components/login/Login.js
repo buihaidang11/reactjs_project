c98179fa1ce3555307eca5e16ecc05f111d10d87
buihaidang11 <dangnb02@gmail.com>
2022-08-04 11:34:18 +0700

    update 2

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Home from '../Home';
 
 const Login = () => {
+
+    const handleLogin = () => {
+
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+    const fetchData = async () =>  {
+        const result = await axios.get('http://localhost:8000/accounts')
+        console.log(result.data);
+    }
+    
     return (
         <div className='cart'>
             <div className='title-cart'>BBS System</div>
             <div className='cart-body'>
                 <form>       
                     <p className='title-input'>E-mail</p>
-                    <input className='input-form'/>
+                    <input className='input-form' name='username' type='text'/>
                     <p className='title-input'>Mật khẩu</p>
-                    <input className='input-form'/>
+                    <input className='input-form' name='password' type='password'/>
                     <div className='nav-form'>
                         <div>
                             <label>
@@ -20,7 +34,7 @@ const Login = () => {
                             </label>
                         </div>
                         <div>
-                            <a>Quên mật khẩu?</a>
+                            <a href='/'>Quên mật khẩu?</a>
                         </div>
                     </div>
                     
@@ -31,7 +45,6 @@ const Login = () => {
                 </form>
 
             </div>
-
         </div>
     );
 };
